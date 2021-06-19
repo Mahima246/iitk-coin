9f1dfacb29ad07235c6246597fc99cfc4dc336d9
Mahima246 <mahimaarora246.m.a@gmail.com>
2021-06-19 19:02:49 +0530

    reward and trasfer endpoints

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"strconv"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
@@ -16,6 +17,7 @@ import (
 )
 type User struct {
 	Name string `json:"Name"`
+	Coins int `json:"Coins"`
 	Rollno     string `json:"Rollno"`
 	Passwd     string `json:"Passwd"`
 	Batch      string `json:"Batch"`
@@ -29,16 +31,39 @@ type auth struct {
 	Passwd string `json:"Passwd"`
 }
 
+type coinrew struct{
+	Rollno string `json:"Rollno"`
+	Coins int `json:"Coins"`
+}
+type cointrans struct{
+	Rollno1 string `json:"Rollno1"`
+	Rollno2 string `json:"Rollno2"`
+	Coins int `json:"Coins"`
+}
+
+type vw struct{
+	Rollno string `json:"Rollno"`
+}
+
 var jwtKey = []byte("my_secret_key")
 
 type Claims struct {
 	Rollno string `json:"Rollno"`
 	jwt.StandardClaims
 }
+
+
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+
+
+
+
+
+
+
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
@@ -104,6 +129,11 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+
+
+
+
+
 func signupHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
@@ -141,6 +171,11 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 	insertauth(sqliteDatabase, &outh)
 }
 
+
+
+
+
+
 func secretpageHandler(w http.ResponseWriter, r *http.Request) {
 	c, err := r.Cookie("token")
 	if err != nil {
@@ -175,6 +210,183 @@ func secretpageHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Welcome to IITK coins %s!", claims.Rollno)))
 }
 
+
+
+
+
+
+func view(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	var vw vw
+	body, _ := ioutil.ReadAll(r.Body)
+	rerr := json.Unmarshal(body, &vw)
+	log.Println(rerr)
+	if rerr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var rollno = vw.Rollno
+	var coin int
+	db, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	res := db.QueryRow("SELECT Coins FROM User WHERE Rollno = ?", rollno).Scan(&coin)
+	
+	if res == sql.ErrNoRows {
+		http.Error(w, "No User found!", http.StatusNotFound)
+		return
+	}
+	
+	if res != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println(res)
+	}
+	s := strconv.Itoa(coin)
+	log.Println(s)
+
+	w.Write([]byte(fmt.Sprintf("Coins in your wallet: %s!", s)))
+}
+
+
+
+
+
+
+
+func reward(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var coin coinrew	
+	body, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(body, &coin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var coins int
+	db, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	res := db.QueryRow("SELECT Coins FROM User WHERE Rollno = ?", coin.Rollno).Scan(&coins)
+	
+	if res == sql.ErrNoRows {
+		http.Error(w, "No User found!", http.StatusNotFound)
+		return
+	}
+	
+	if res != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println(res)
+	}
+
+	coins = coins + coin.Coins
+
+	log.Println(coins)
+	statement, err := db.Prepare("UPDATE User SET Coins = ? WHERE Rollno = ?")
+	
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(coins,coin.Rollno)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+
+
+
+
+
+
+func transfer(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	var cointr cointrans	
+	body, _ := ioutil.ReadAll(r.Body)
+	err := json.Unmarshal(body, &cointr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var coins1 int
+	var coins2 int
+	db, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	res := db.QueryRow("SELECT Coins FROM User WHERE Rollno = ?", cointr.Rollno1).Scan(&coins1)
+	res2 := db.QueryRow("SELECT Coins FROM User WHERE Rollno = ?", cointr.Rollno2).Scan(&coins2)
+	
+	if res == sql.ErrNoRows {
+		http.Error(w, "1st User Not found!", http.StatusNotFound)
+		return
+	}
+	
+	if res != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println(res)
+	}
+	if res2 == sql.ErrNoRows {
+		http.Error(w, "1st User Not found!", http.StatusNotFound)
+		return
+	}
+	
+	if res2 != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println(res2)
+	}
+
+	if coins1<cointr.Coins{
+		http.Error(w, "uhoh, User 1 doesn't have enough coins to transfer to user 2", http.StatusBadRequest)
+		log.Fatalln("User 1 doesn't have enough coins to transfer to user 2")
+	}
+	statement1, err1 := db.Prepare("UPDATE User SET Coins = ? WHERE Rollno = ?")
+	
+	if err1 != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Fatalln(err1.Error())
+	}
+	_, err1 = statement1.Exec(coins1-cointr.Coins,cointr.Rollno1)
+	if err1 != nil {
+		log.Fatalln(err1.Error())
+	}
+
+	statement, err := db.Prepare("UPDATE User SET Coins = ? WHERE Rollno = ?")
+	
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Fatalln(err.Error())
+	}
+	_, err = statement.Exec(coins2+cointr.Coins,cointr.Rollno2)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+}
+
+
+
+
+
+
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/signup", signupHandler).
@@ -183,7 +395,13 @@ func main() {
 		Methods("POST")
 	r.HandleFunc("/secretpage", secretpageHandler).
 		Methods("GET")
-
+	r.HandleFunc("/reward",reward).
+		Methods("POST")
+	r.HandleFunc("/view",view).
+		Methods("GET")
+	r.HandleFunc("/transfer",transfer).
+		Methods("POST")
+	
 	file, err := os.Create("sqlite-database.db") // Creating SQLite database file
 	if err != nil {
 		log.Fatal(err.Error())
@@ -203,6 +421,7 @@ func createTable(db *sql.DB) {
 		Name TEXT,
 		Passwd TEXT,
 		Rollno TEXT,
+		Coins INTEGER,
 		Batch TEXT,
 		EventsPart TEXT,
 		IsAdmin INTEGER,
@@ -224,14 +443,16 @@ func createTable(db *sql.DB) {
 	}
 	st.Exec()
 }
+
+
 func insertUser(db *sql.DB, u *User) {
 
-	insertUserSQL := `INSERT INTO User( Name, Passwd ,Rollno, Batch, EventsPart, IsAdmin, IsCordi) VALUES (?, ?, ?, ?, ?, ?, ?)`
+	insertUserSQL := `INSERT INTO User( Name, Passwd ,Rollno, Coins, Batch, EventsPart, IsAdmin, IsCordi) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`
 	statement, err := db.Prepare(insertUserSQL)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	_, err = statement.Exec(u.Name, u.Passwd, u.Rollno, u.Batch, u.EventsPart, u.IsAdmin, u.Iscordi)
+	_, err = statement.Exec(u.Name, u.Passwd, u.Rollno, u.Coins, u.Batch, u.EventsPart, u.IsAdmin, u.Iscordi)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
